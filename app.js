02eb4651101fb718b844306d365d73acf40aaf7a
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-12-12 19:53:44 +0200

    add function

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+
+
+
 let view = {
     displayMessage: function (message) {
-        
+        let gameMessage = document.querySelector(".text-result")
+       return gameMessage.innerHTML = message
     },
     displayHit: function (location) {
-        
+        let gameHit = document.getElementById(location)
+        gameHit.classList.add("hit")
     },
     displayMiss: function (location) {
-        
+        let gameMiss = document.getElementById(location)
+        gameMiss.classList.add("miss")
     },
 
 }
+view.displayMessage("START GAME")
+view.displayHit("33")
+view.displayMiss("34")
+
