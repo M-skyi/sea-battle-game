b4c5e42b64e9535afdd583a376c009e85653fd9e
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-12-17 21:47:35 +0200

    add script

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ let view = {
         gameMiss.classList.add("miss")
     },
 }
-
-// view.displayMessage("START GAME")
-// view.displayHit("33")
-// view.displayMiss("34")
-view.displayHit("31")
-view.displayHit("32")
-view.displayHit("33")
-
 let model = {
     boardSize: 7,
     numShips: 3,
@@ -40,15 +32,46 @@ let model = {
     ],
     fire: function (guess) {
         for (let i = 0; i < this.numShips; i++) {
-            let ship = this.ships[i]
-            let locations = ship.locations
-            let hits = ship.hits
-            let indexGuess = locations.indexOf(guess)
-            if (locations.includes(guess) == true) {
-                hits.splice(indexGuess, 1, 'hit');
+            let ship = this.ships[i];
+            let indexGuess = ship.locations.indexOf(guess);
+            if (indexGuess >= 0) {
+                ship.hits[indexGuess] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("hit")
+                if (this.isSunk(ship)) {
+                    this.shipsSunk++
+                    if (this.shipsSunk === 1) {
+                        view.displayMessage(`You sank ${this.shipsSunk} my ship !`)
+                    }
+                    if (this.shipsSunk > 1) {
+                        view.displayMessage(`You sank ${this.shipsSunk} of my ships !`)
+                    }
+                    if (this.shipsSunk === 3) {
+                        view.displayMessage(`You sank ${this.shipsSunk} of my ships! GAME OVER`)
+                    }
+                }
+                return true
+            }if (indexGuess < 0 && indexGuess !== 0 ) {
+                view.displayMiss(guess)
+                view.displayMessage("miss")
             }
-            console.log(hits)
         }
+        return false
+    },
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false
+            }
+        }
+        return true
     }
 }
-model.fire("52")
+model.fire("30")
+model.fire("31")
+model.fire("32")
+model.fire("33")
+
+
+
+
